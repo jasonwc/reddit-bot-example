e8a908635856d718c8eae75a1544b47fdc515c42
Jason Carter <jason@mavenlink.com>
2018-02-05 23:36:18 -0700

    Format the message we send to slack.

--- reddit-bot/commands/reddit.rb
+++ reddit-bot/commands/reddit.rb
@@ -14,25 +14,62 @@ module RedditBot
         # Ensure we don't try to pass an invalid sort order
         sort_by = ['hot', 'controversial', 'top', 'rising', 'new'].include?(sort_by) ? sort_by : 'hot'
 
-        client.say(channel: data.channel, text: "Grabbing #{sort_by} posts from /r/#{subreddit}...")
-
         # Get the data from Reddit and format as JSON
         uri = URI("https://www.reddit.com/r/#{subreddit}/#{sort_by}.json")
         response = JSON.parse(Net::HTTP.get(uri))
 
-        # Grab the first 3 posts, and get the information we care about
+        # # Grab the first 3 posts, and get the information we care about
         posts = response['data']['children'].first(3).map do |post|
           {
             url: post['data']['url'],
             title: post['data']['title'],
-            score: post['data']['score']
+            score: post['data']['score'],
+            permalink: post['data']['permalink'],
+            self_post: post['data']['is_self'],
+            body: post['data']['self_text'],
+            subreddit: post['data']['subreddit'],
+            num_comments: post['data']['num_comments'],
           }
         end
 
-        # Message the channel for each post
-        posts.each do |post|
-          client.say(channel: data.channel, text: "Title: #{post[:title]}, Link: #{post[:url]}, Score: #{post[:score]}")
-        end
+        # Format the message
+        message = {
+          "text": "Here are the first 3 #{sort_by} posts on /r/#{subreddit}",
+          "attachments": posts.map do |post|
+            {
+              "fallback": post[:title],
+              "color": "#ff5700",
+              "title": post[:title],
+              "title_link": "http://www.reddit.com#{post[:permalink]}",
+              "text": post[:self_post] ? post[:body] : post[:url],
+              "fields": [
+                {
+                  "title": "Type",
+                  "value": post[:self_post] ? 'Self' : 'Link',
+                  "short": true
+                },
+                {
+                  "title": "Subreddit",
+                  "value": "/r/#{post[:subreddit]}",
+                  "short": true
+                },
+                {
+                  "title": "Score",
+                  "value": post[:score],
+                  "short": true
+                },
+                {
+                  "title": "Comments",
+                  "value": post[:num_comments],
+                  "short": true
+                }
+              ]
+            }
+          end
+        }
+
+        # Message the channel with a formatted message using the web client
+        client.web_client.chat_postMessage(channel: data.channel, as_user: true, **message)
       end
     end
   end
