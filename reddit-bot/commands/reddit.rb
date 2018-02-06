7d9405243f87c63756234f5b5d012a56fb230457
Jason Carter <jason@mavenlink.com>
2018-02-05 22:39:45 -0700

    Allow a user to specify a subreddit from which to grab posts.

--- reddit-bot/commands/reddit.rb
+++ reddit-bot/commands/reddit.rb
@@ -3,9 +3,15 @@ require 'net/http'
 module RedditBot
   module Commands
     class Reddit < SlackRubyBot::Commands::Base
-      command 'reddit' do |client, data, _match|
+      BASE_URL = "https://www.reddit.com/r/"
+
+      command 'reddit' do |client, data, match|
+        # Grab the subreddit from the command, or default to all
+        subreddit = match[:expression] || 'all'
+        client.say(channel: data.channel, text: "Grabbing posts from /r/#{subreddit}...")
+
         # Get the data from Reddit and format as JSON
-        uri = URI('https://www.reddit.com/r/all.json')
+        uri = URI("https://www.reddit.com/r/#{subreddit}.json")
         response = JSON.parse(Net::HTTP.get(uri))
 
         # Grab the first 3 posts, and get the information we care about
