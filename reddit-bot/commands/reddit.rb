ff2aad8f162deafa930a638d40efdc62019c108e
Jason Carter <jason@mavenlink.com>
2018-02-05 22:52:52 -0700

    Allow a user to choose a sort order, but default to hot if invalid or none passed.

--- reddit-bot/commands/reddit.rb
+++ reddit-bot/commands/reddit.rb
@@ -1,5 +1,4 @@
 require 'net/http'
-require 'pry'
 
 module RedditBot
   module Commands
@@ -7,12 +6,18 @@ module RedditBot
       BASE_URL = "https://www.reddit.com/r/"
 
       command 'reddit' do |client, data, match|
-        # Grab the subreddit from the command, or default to all
-        subreddit = match[:expression] || 'all'
-        client.say(channel: data.channel, text: "Grabbing posts from /r/#{subreddit}...")
+        # Grab the subreddit and sort order from the command, or choose defaults
+        args = match[:expression].split(' ')
+        subreddit = args.try(:[], 0) || 'all'
+        sort_by = args.try(:[], 1) || 'hot'
+
+        # Ensure we don't try to pass an invalid sort order
+        sort_by = ['hot', 'controversial', 'top', 'rising', 'new'].include?(sort_by) ? sort_by : 'hot'
+
+        client.say(channel: data.channel, text: "Grabbing #{sort_by} posts from /r/#{subreddit}...")
 
         # Get the data from Reddit and format as JSON
-        uri = URI("https://www.reddit.com/r/#{subreddit}.json")
+        uri = URI("https://www.reddit.com/r/#{subreddit}/#{sort_by}.json")
         response = JSON.parse(Net::HTTP.get(uri))
 
         # Grab the first 3 posts, and get the information we care about
