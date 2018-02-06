15858fe06e617fa97ad9be02b7492577d372f7f2
Jason Carter <jason@mavenlink.com>
2018-02-06 00:53:14 -0700

    Fix command description.

--- reddit-bot/bot.rb
+++ reddit-bot/bot.rb
@@ -5,7 +5,7 @@ module RedditBot
       desc 'Grabs posts from Reddit!'
 
       command 'reddit' do
-        desc 'Returns back the top 3 posts for a given subreddit. `@reddit-bot reddit <subreddit>`'
+        desc 'Returns back 3 sorted posts for a given subreddit. `@reddit-bot reddit <subreddit> <top|new|controversial|rising|hot>`'
       end
     end
   end
